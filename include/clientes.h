7856564251228d03124d61b0949d446b5395cc58
Edupizzol <edupizzol16@gmail.com>
2026-01-27 21:38:36 -0300

    adição da função copy_string, e criar_cliente

--- include/clientes.h
+++ include/clientes.h
@@ -13,6 +13,7 @@ typedef struct NodeCliente{
 }NodeCliente;
 
 Cliente* criar_cliente(char* nome, char* cpf, char* telefone);
+char* copy_string(char* new_string, char* string);
 void cadastrar_cliente(NodeCliente** lista, char* nome, char* cpf, char* telefone);
 void listar_cliente(NodeCliente *root);
 Cliente* busca_cliente(NodeCliente *root, char* cpf);
--- src/clientes.c
+++ src/clientes.c
@@ -2,3 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* copy_string(char* string){
+    
+    int i=0;
+    int memory_size=0;
+    char* new_string = NULL;
+
+    while(string[i]!='\0'){
+
+        if(i==memory_size){
+
+            memory_size+=10;
+
+            char* temp = realloc(new_string,memory_size*sizeof(char));
+            if(temp==NULL){
+                printf("Erro durante a alocação de memória do nome do cliente!\n");
+                exit(EXIT_FAILURE);
+            }
+
+            new_string = temp;
+
+            }
+
+        new_string[i]=string[i];
+        i++;
+
+    }
+
+    char* temp = realloc(new_string,i+1);
+    if(temp==NULL){
+        printf("Erro durante a alocação de memória do nome do cliente!\n");
+        exit(EXIT_FAILURE);
+    }
+    new_string = temp;
+    
+    new_string[i]='\0';
+    return new_string;
+
+}
+
+Cliente* criar_cliente(char* nome, char* cpf, char* telefone){
+
+    Cliente* new_cliente = malloc(sizeof(Cliente));
+    if(new_cliente==NULL){
+        printf("Erro ao alocar memória para Cliente!");
+        exit(EXIT_FAILURE);
+    }
+
+    new_cliente->cpf = copy_string(cpf);
+    new_cliente->nome = copy_string(nome);
+    new_cliente->telefone = copy_string(telefone);
+
+    return new_cliente;
+    
+}
