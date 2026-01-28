61f886e57ce8ebc96a6379e8823fcbae29830e22
Edupizzol <edupizzol16@gmail.com>
2026-01-27 21:04:23 -0300

    adicionando o protótipo das funções

--- include/clientes.h
+++ include/clientes.h
@@ -1,3 +1,6 @@
+#ifndef CLIENTES_H
+#define CLIENTES_H
+
 typedef struct Cliente{
     char* nome;
     char* cpf;
@@ -9,3 +12,11 @@ typedef struct NodeCliente{
     struct NodeCliente* prox;
 }NodeCliente;
 
+Cliente* criar_cliente(char* nome, char* cpf, char* telefone);
+void cadastrar_cliente(NodeCliente** lista, char* nome, char* cpf, char* telefone);
+void listar_cliente(NodeCliente *root);
+Cliente* busca_cliente(NodeCliente *root, char* cpf);
+void editar_cliente(NodeCliente *root, char* cpf);
+void removercliente(NodeCliente *root, char* cpf);
+
+#endif
